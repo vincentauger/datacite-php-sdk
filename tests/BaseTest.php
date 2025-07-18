5b1afd4167214cf0d1e2a9556a69b6b62165482e
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2025-07-18 16:24:58 -0300

    ci: update test framework

---
+++ tests/BaseTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTest extends TestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+
+        // This method is called once before any tests in the class are run.
+        // You can use it to set up any shared resources needed for the tests.
+
+        parent::setUpBeforeClass();
+    }
+}
